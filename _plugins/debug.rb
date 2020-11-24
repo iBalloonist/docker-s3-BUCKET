0376a491497a855727e3e7b2ce4c2ceff1899c7f
iBalloonist <106442403+iBalloonist@users.noreply.github.com>
2020-11-24 23:03:29 +0000

    s5h1420: fix a buffer overflow when checking userspace params

--- _plugins/debug.rb
+++ _plugins/debug.rb
@@ -1,3 +1,9 @@
 # A simple way to inspect liquid template variables.
 # Usage:
-#  C
+#  Can be used anywhere liquid syntax is parsed (templates, includes, posts/pages)
+#  {{ site | debug }}
+#  {{ site.posts | debug }}
+#
+require 'pp'
+module Jekyll
+  # Need to overwrite the inspect method here be
